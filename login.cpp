b0c27e59f07d19a1e2116853175d5f7698571971
Moeez-A <f20232661163@umt.edu.pk>
2024-07-20 02:54:11 +0500

    made short menu

--- login.cpp
+++ login.cpp
@@ -9,6 +9,19 @@ So, to summarize:
 #include<string>
 using namespace std;
 
-login::login(){
-
+login::login():password(){
+    int choice;
+        void askLogin(int& selectedAccount) {
+        cout << "------------------------------------" << endl;
+        cout << "Welcome to the Cinema Management System" << endl;
+        cout << "------------------------------------" << endl;
+        cout << "To login to your already existing user account, enter '1'" << endl;
+        cout << "To create a new user account, enter '2'" << endl;
+        cout << "To login as a System admin enter '3' " <<endl;
+        cout << "To login as a guest enter '4' " <<endl;
+        cout << "To exit the program, enter '0'" << endl;
+        cout << "Enter your choice: ";
+        cin >> selectedAccount;
+        system("cls");
+}
 };
